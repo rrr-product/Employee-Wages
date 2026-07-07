a1737ef649b18a753b45aa4dfd54e8c2db4aa058
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-07-07 14:52:34 +0530

    [Rishi Babu] UC-5 Code for calculating employee wages for 20 days is implemented

--- employeeWagse.js
+++ employeeWagse.js
@@ -3,27 +3,39 @@ console.log("Welcome to Employee Wage Computation Program");
 const EMPLOYEE_RATE_PER_HOUR = 20;
 const FULL_DAY_HOURS = 8;
 const PART_TIME_HOURS = 4;
+const TOTAL_WORKING_DAYS = 20;
 
 let employeeType = Math.floor(Math.random() * 2) + 1;
-let employeeCheck = Math.floor(Math.random() * 2);
+let totalWage = 0;
+console.log(`Employee Type : ${employeeType === 1 ? "Full Time" : "Part Time"}`);
 
-if (employeeCheck) {
-    switch (employeeType) {
-        case 1:
-            let fullTimeWage = EMPLOYEE_RATE_PER_HOUR * FULL_DAY_HOURS;
-            console.log("Status : Present");
-            console.log("Employee Type : Full Time");
-            console.log(`Employee Wage is: ${fullTimeWage}`);
-            break;
-        case 2:
-            let partTimeWage = EMPLOYEE_RATE_PER_HOUR * PART_TIME_HOURS;
-            console.log("Status : Present");
-            console.log("Employee Type : Part Time");
-            console.log(`Employee Wage is: ${partTimeWage}`);
-            break;
-    }    
-} else {
-    console.log("Status : Absent");
-    console.log(`Employee Type : ${employeeType === 1 ? "Full Time" : "Part Time"}`);
-    console.log("Employee Wage is: 0");
+const totalEmolpyeeWages = (employeeType) => {
+    let employeeCheck = Math.floor(Math.random() * 2);
+    let totalWage = 0;
+    if (employeeCheck) {
+        switch (employeeType) {
+            case 1:
+                let fullTimeWage = EMPLOYEE_RATE_PER_HOUR * FULL_DAY_HOURS;
+                totalWage += fullTimeWage;
+                console.log("Status : Present");
+                console.log(`Employee Wage is: ${fullTimeWage}`);
+                break;
+            case 2:
+                let partTimeWage = EMPLOYEE_RATE_PER_HOUR * PART_TIME_HOURS;
+                totalWage += partTimeWage;
+                console.log("Status : Present");
+                console.log(`Employee Wage is: ${partTimeWage}`);
+                break;
+        }    
+    } else {
+        console.log("Status : Absent");
+        console.log("Employee Wage is: 0");
+    }
+    return totalWage;
 }
+
+for (let day = 1; day <= TOTAL_WORKING_DAYS; day++) {
+    totalWage += totalEmolpyeeWages(employeeType);
+}
+
+console.log(`Total Wage for the month: ${totalWage}`);
