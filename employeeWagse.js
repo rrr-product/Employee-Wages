12b7a7904ada75b8c0685bdb40011525b877c5b8
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-07-07 12:26:14 +0530

    [Rishi Babu] UC-3 Part time employies working hour is included

--- employeeWagse.js
+++ employeeWagse.js
@@ -2,13 +2,25 @@ console.log("Welcome to Employee Wage Computation Program");
 
 const EMPLOYEE_RATE_PER_HOUR = 20;
 const FULL_DAY_HOURS = 8;
+const PART_TIME_HOURS = 4;
 
+let employeeType = Math.floor(Math.random() * 2) + 1;
 let employeeCheck = Math.floor(Math.random() * 2);
 
 if (employeeCheck) {
-    let employeeWage = EMPLOYEE_RATE_PER_HOUR * FULL_DAY_HOURS;
-    console.log("Employee is Present");
-    console.log("Employee Wage is: " + employeeWage);
+    if (employeeType === 1) {
+        let employeeWage = EMPLOYEE_RATE_PER_HOUR * FULL_DAY_HOURS;
+        console.log("Status : Present");
+        console.log("Employee Type : Full Time");
+        console.log(`Employee Wage is: ${employeeWage}`);
+    } else {
+        let employeeWage = EMPLOYEE_RATE_PER_HOUR * PART_TIME_HOURS;
+        console.log("Status : Present");
+        console.log("Employee Type : Part Time");
+        console.log(`Employee Wage is: ${employeeWage}`);
+    }
 } else {
-    console.log("Employee is Absent");
+    console.log("Status : Absent");
+    console.log(`Employee Type : ${employeeType === 1 ? "Full Time" : "Part Time"}`);
+    console.log("Employee Wage is: 0");
 }
