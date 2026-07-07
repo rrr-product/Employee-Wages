7e7c2cc25666fd2868c94893fd116762835192a3
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-07-07 12:16:58 +0530

    [Rishi Babu] UC-2 Code to calculate employee wages per day is implemented.

--- employeeWagse.js
+++ employeeWagse.js
@@ -1,5 +1,14 @@
 console.log("Welcome to Employee Wage Computation Program");
 
+const EMPLOYEE_RATE_PER_HOUR = 20;
+const FULL_DAY_HOURS = 8;
+
 let employeeCheck = Math.floor(Math.random() * 2);
-let result = `Employee is ${employeeCheck ? "Present" : "Absent"}`;
-console.log(result);
+
+if (employeeCheck) {
+    let employeeWage = EMPLOYEE_RATE_PER_HOUR * FULL_DAY_HOURS;
+    console.log("Employee is Present");
+    console.log("Employee Wage is: " + employeeWage);
+} else {
+    console.log("Employee is Absent");
+}
