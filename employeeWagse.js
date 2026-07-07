0906694f219120cef4cabcac03c695e4de02e168
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-07-07 12:31:54 +0530

    [Rishi Babu] UC-4 The code is solved using switch case.

--- employeeWagse.js
+++ employeeWagse.js
@@ -8,17 +8,20 @@ let employeeType = Math.floor(Math.random() * 2) + 1;
 let employeeCheck = Math.floor(Math.random() * 2);
 
 if (employeeCheck) {
-    if (employeeType === 1) {
-        let employeeWage = EMPLOYEE_RATE_PER_HOUR * FULL_DAY_HOURS;
-        console.log("Status : Present");
-        console.log("Employee Type : Full Time");
-        console.log(`Employee Wage is: ${employeeWage}`);
-    } else {
-        let employeeWage = EMPLOYEE_RATE_PER_HOUR * PART_TIME_HOURS;
-        console.log("Status : Present");
-        console.log("Employee Type : Part Time");
-        console.log(`Employee Wage is: ${employeeWage}`);
-    }
+    switch (employeeType) {
+        case 1:
+            let fullTimeWage = EMPLOYEE_RATE_PER_HOUR * FULL_DAY_HOURS;
+            console.log("Status : Present");
+            console.log("Employee Type : Full Time");
+            console.log(`Employee Wage is: ${fullTimeWage}`);
+            break;
+        case 2:
+            let partTimeWage = EMPLOYEE_RATE_PER_HOUR * PART_TIME_HOURS;
+            console.log("Status : Present");
+            console.log("Employee Type : Part Time");
+            console.log(`Employee Wage is: ${partTimeWage}`);
+            break;
+    }    
 } else {
     console.log("Status : Absent");
     console.log(`Employee Type : ${employeeType === 1 ? "Full Time" : "Part Time"}`);
