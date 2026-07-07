2fa4a8729e27c7ddde8361ef7f4a12a14bbc4dbe
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-07-07 12:11:06 +0530

    [Rishi Babu] UC-1 Code to check the presence of employee is implemented

--- employeeWagse.js
+++ employeeWagse.js
@@ -1,1 +1,5 @@
 console.log("Welcome to Employee Wage Computation Program");
+
+let employeeCheck = Math.floor(Math.random() * 2);
+let result = `Employee is ${employeeCheck ? "Present" : "Absent"}`;
+console.log(result);
