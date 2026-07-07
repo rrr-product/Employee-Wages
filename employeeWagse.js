475d9494a679261a25ceac37cd793281c3b9d341
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-07-07 15:57:56 +0530

    [Rishi Babu] UC-6 Employee salary will be calculated for 100 hours and 20 days

--- employeeWagse.js
+++ employeeWagse.js
@@ -7,22 +7,26 @@ const TOTAL_WORKING_DAYS = 20;
 
 let employeeType = Math.floor(Math.random() * 2) + 1;
 let totalWage = 0;
+let totalWorkingHours = 0;
 console.log(`Employee Type : ${employeeType === 1 ? "Full Time" : "Part Time"}`);
 
-const totalEmolpyeeWages = (employeeType) => {
+const totalEmolpyeeWages = (employeeType, totalWorkingHours) => {
     let employeeCheck = Math.floor(Math.random() * 2);
+    let totalWorkHour = totalWorkingHours
     let totalWage = 0;
     if (employeeCheck) {
         switch (employeeType) {
             case 1:
                 let fullTimeWage = EMPLOYEE_RATE_PER_HOUR * FULL_DAY_HOURS;
                 totalWage += fullTimeWage;
+                totalWorkingHours += FULL_DAY_HOURS;
                 console.log("Status : Present");
                 console.log(`Employee Wage is: ${fullTimeWage}`);
                 break;
             case 2:
                 let partTimeWage = EMPLOYEE_RATE_PER_HOUR * PART_TIME_HOURS;
                 totalWage += partTimeWage;
+                totalWorkingHours += PART_TIME_HOURS;
                 console.log("Status : Present");
                 console.log(`Employee Wage is: ${partTimeWage}`);
                 break;
@@ -31,11 +35,16 @@ const totalEmolpyeeWages = (employeeType) => {
         console.log("Status : Absent");
         console.log("Employee Wage is: 0");
     }
-    return totalWage;
+    return {totalWage, totalWorkingHours};
 }
 
+
 for (let day = 1; day <= TOTAL_WORKING_DAYS; day++) {
-    totalWage += totalEmolpyeeWages(employeeType);
+    if (totalWorkingHours >= 100) break;
+    let result = totalEmolpyeeWages(employeeType, totalWorkingHours);
+    totalWage += result.totalWage;
+    totalWorkingHours = result.totalWorkingHours;
 }
 
 console.log(`Total Wage for the month: ${totalWage}`);
+console.log(`Total Working Hours for the month: ${totalWorkingHours}`);
