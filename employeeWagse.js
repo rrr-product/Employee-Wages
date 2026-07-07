a9e8ddff5fa702570a512817edba3f44a0613040
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-07-07 16:35:30 +0530

    [Rishi Babu] UC-7 Code is refactured according to class and functions to calculate employee wages.

--- employeeWagse.js
+++ employeeWagse.js
@@ -1,49 +1,57 @@
 console.log("Welcome to Employee Wage Computation Program");
 
-const EMPLOYEE_RATE_PER_HOUR = 20;
-const FULL_DAY_HOURS = 8;
-const PART_TIME_HOURS = 4;
-const TOTAL_WORKING_DAYS = 20;
+class EmployeeWages {
 
-let employeeType = Math.floor(Math.random() * 2) + 1;
-let totalWage = 0;
-let totalWorkingHours = 0;
-console.log(`Employee Type : ${employeeType === 1 ? "Full Time" : "Part Time"}`);
-
-const totalEmolpyeeWages = (employeeType, totalWorkingHours) => {
-    let employeeCheck = Math.floor(Math.random() * 2);
-    let totalWorkHour = totalWorkingHours
-    let totalWage = 0;
-    if (employeeCheck) {
-        switch (employeeType) {
-            case 1:
-                let fullTimeWage = EMPLOYEE_RATE_PER_HOUR * FULL_DAY_HOURS;
-                totalWage += fullTimeWage;
-                totalWorkingHours += FULL_DAY_HOURS;
-                console.log("Status : Present");
-                console.log(`Employee Wage is: ${fullTimeWage}`);
-                break;
-            case 2:
-                let partTimeWage = EMPLOYEE_RATE_PER_HOUR * PART_TIME_HOURS;
-                totalWage += partTimeWage;
-                totalWorkingHours += PART_TIME_HOURS;
-                console.log("Status : Present");
-                console.log(`Employee Wage is: ${partTimeWage}`);
-                break;
-        }    
-    } else {
-        console.log("Status : Absent");
-        console.log("Employee Wage is: 0");
+    constructor() {
+        this.EMPLOYEE_RATE_PER_HOUR = 20;
+        this.FULL_DAY_HOURS = 8;
+        this.PART_TIME_HOURS = 4;
+        this.TOTAL_WORKING_DAYS = 20;
+        this.employeeType = Math.floor(Math.random() * 2) + 1;
+        this.totalWage = 0;
+        this.totalWorkingHours = 0;
+        console.log(`Employee Type : ${this.employeeType === 1 ? "Full Time" : "Part Time"}`);
     }
-    return {totalWage, totalWorkingHours};
+
+    totalEmolpyeeWages = (employeeType, totalWorkingHours) => {
+        let employeeCheck = Math.floor(Math.random() * 2);
+        let totalWorkHour = totalWorkingHours
+        let totalWage = 0;
+        if (employeeCheck) {
+            switch (employeeType) {
+                case 1:
+                    let fullTimeWage = this.EMPLOYEE_RATE_PER_HOUR * this.FULL_DAY_HOURS;
+                    totalWage += fullTimeWage;
+                    totalWorkingHours += this.FULL_DAY_HOURS;
+                    console.log("Status : Present");
+                    console.log(`Employee Wage is: ${fullTimeWage}`);
+                    break;
+                case 2:
+                    let partTimeWage = this.EMPLOYEE_RATE_PER_HOUR * this.PART_TIME_HOURS;
+                    totalWage += partTimeWage;
+                    totalWorkingHours += this.PART_TIME_HOURS;
+                    console.log("Status : Present");
+                    console.log(`Employee Wage is: ${partTimeWage}`);
+                    break;
+            }    
+        } else {
+            console.log("Status : Absent");
+            console.log("Employee Wage is: 0");
+        }
+        return {totalWage, totalWorkingHours};
+    }
+
 }
 
+let totalWorkingHours = 0;
+let totalWage = 0;
+let employeeWages = new EmployeeWages();
 
-for (let day = 1; day <= TOTAL_WORKING_DAYS; day++) {
+for (let day = 1; day <= 20; day++) {
     if (totalWorkingHours >= 100) break;
-    let result = totalEmolpyeeWages(employeeType, totalWorkingHours);
+    let result = employeeWages.totalEmolpyeeWages(employeeWages.employeeType, employeeWages.totalWorkingHours);
     totalWage += result.totalWage;
-    totalWorkingHours = result.totalWorkingHours;
+    totalWorkingHours += result.totalWorkingHours;
 }
 
 console.log(`Total Wage for the month: ${totalWage}`);
